7ee6d1e5f8dd467357984b70693a4fcbc580a2dc
Ezzerof <christian.bitca@gmail.com>
2023-03-24 19:01:46 +0000

    BubbleSort class added flags on the sort method

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -3,34 +3,33 @@ package BubbleSort;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static int[] sort(int[] numbers) {
+    public static void sort(int[] numbers) {
 
-        if (numbers == null)
-            return new int[]{0};
+        boolean isSorted = false;
 
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] > numbers[j]) {
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+        for (int i = 0; i < numbers.length && !isSorted; i++) {
+            isSorted = true;
+            for (int j = 1; j < numbers.length - i; j++) {
+                if (numbers[j] < numbers[j - 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = temp;
+                    isSorted = false;
                 }
             }
         }
-        return numbers;
     }
 
 
 
     public static void main(String[] args) {
 //        int[] numsNull = null;
-        int[] nums = {-2,-3,-89,0,1,-21};
-//        int[] numsNeg = {10,9,8,1,2,3,4,5};
+        int[] nums = {1,2,3,4,5,6};
+//        int[] nums = {10,9,8,1,2,3,4,5};
 //        int[] empty = {};
         System.out.println("Unsorted array: " + Arrays.toString(nums));
-
-        int[] res = sort(nums);
-        System.out.println("Sorted array: " + Arrays.toString(res));
+        sort(nums);
+        System.out.println("Sorted array: " + Arrays.toString(nums));
 
     }
 }
